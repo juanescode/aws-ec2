c513432a6c45dc65944c38fc191231602ba5c81d
juanescode <juanestebanvilladagallego23@gmail.com>
2024-08-13 14:12:34 -0500

    port 8080

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hola mundo');
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 })
